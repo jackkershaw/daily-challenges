ba17bed20c273d51e46d190993c816baf347b318
jones58 <108466184+jones58@users.noreply.github.com>
2023-11-18 10:59:10 +0000

    button click sets local storage check this on load

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,45 @@ let currentDate = new Date().toLocaleString("default", {
   month: "long",
   day: "numeric",
 });
+
 // date showing on page //
 const dateOnPage = document.getElementById("date");
 dateOnPage.textContent = currentDate;
 
-// buttons going green when clicked//
+// getting all buttons //
 const buttons = document.querySelectorAll("button");
+
+// buttons going green when clicked and set local storage to button id and today's date//
 buttons.forEach(function (button) {
   button.addEventListener("click", () => {
-    button.clicked = true;
+    if (button.id !== "reset") {
+      localStorage.setItem(button.id, currentDate);
+    }
     if (!button.classList.contains("noColorChange")) {
       button.style.backgroundColor = "#608B54";
     }
   });
 });
+
+// reset button and reset local storage for button based on id//
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+  buttons.forEach(function (button) {
+    localStorage.removeItem(button.id);
+    if (!button.classList.contains("noColorChange")) {
+      button.style.backgroundColor = "#3a3a3c";
+    }
+  });
+});
+
+// checking if button has been clicked today on page load //
+window.onload = function () {
+  buttons.forEach(function (button) {
+    let storedDate = localStorage.getItem(button.id);
+    if (storedDate === currentDate) {
+      if (!button.classList.contains("noColorChange")) {
+        button.style.backgroundColor = "#608B54";
+      }
+    }
+  });
+};
